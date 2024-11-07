2f83f59ee9e297c04820e618835efb4527d5ee27
Feezca <fecarrizzo@gmail.com>
2024-11-07 00:41:57 -0300

    feature: carousel en seccion principal

--- src/components/Products/FeaturedProduct.jsx
+++ src/components/Products/FeaturedProduct.jsx
@@ -5,6 +5,9 @@ import { Link } from "react-router-dom";
 import useLanguage from "../themes/useLanguage"; // Importar el hook personalizado
 import { getAllProducts } from "../../api/productService";
 import { Spinner } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 
 const FeaturedProduct = () => {
   const { darkMode } = useContext(ThemeContext);
@@ -12,13 +15,30 @@ const FeaturedProduct = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        console.log("Fetching all products...");
         const data = await getAllProducts();
-        console.log("Fetched products:", data);
-        setProducts(data.slice(0, 3));
+        setProducts(data.slice(0, 8));
       } catch (error) {
         console.error("Error fetching products:", error);
       } finally {
@@ -55,7 +75,7 @@ const FeaturedProduct = () => {
       >
         {t.featuredProducts}
       </p>
-      <div className="d-flex justify-content-around flex-wrap">
+      <Carousel responsive={responsive} infinite autoPlay>
         {products.map((product) => (
           <div key={product.id} className="d-flex justify-content-center mb-4">
             <div style={{ width: "20rem" }}>
@@ -63,7 +83,7 @@ const FeaturedProduct = () => {
             </div>
           </div>
         ))}
-      </div>
+      </Carousel>
       <div className="text-end mt-4 me-4 link-opacity-100-hover">
         <Link
           to="/all"
